cbc507759304fd956cfa0a931fcc4b097cbcf321
folarin0 <160610624+folarin0@users.noreply.github.com>
2024-02-22 12:20:21 +0000

    Update main.blocks, main.ts, tilemap.g.jres, tilemap.g.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,18 @@
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hops_and_paw.vy == 0) {
+        hops_and_paw.vy = -150
+    }
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile4`, function (sprite, location) {
+    game.gameOver(true)
+})
+scene.onOverlapTile(SpriteKind.Player, assets.tile`myTile3`, function (sprite, location) {
+    game.gameOver(false)
+    game.setGameOverEffect(false, effects.melt)
+})
+let hops_and_paw: Sprite = null
 scene.setBackgroundColor(9)
-let hops_and_paw = sprites.create(img`
+hops_and_paw = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
@@ -17,5 +30,7 @@ let hops_and_paw = sprites.create(img`
     . . . . . . . . . . . . . . . . 
     . . . . . . . . . . . . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(hops_and_paw)
+controller.moveSprite(hops_and_paw, 100, 0)
 tiles.setCurrentTilemap(tilemap`level1`)
+hops_and_paw.ay = 350
+scene.cameraFollowSprite(hops_and_paw)
